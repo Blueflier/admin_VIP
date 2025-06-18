8532045fcc4d0c1fe066442fc08185e3b007aa34
Joseph Hartono <joseph.e.hartono@gmail.com>
2025-06-18 15:21:03 -0700

    config

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.example.js
+// INSTRUCTIONS:
+// 1. Go to https://firebase.google.com/
+// 2. Create a new project or use an existing one
+// 3. Go to Project Settings > General tab
+// 4. In "Your apps" section, click "Add app" and select "Web"
+// 5. Copy the config values below and replace them with your actual values
+// 6. Rename this file to "firebase-config.js" (remove the .example)
+
+
+  
+
+// Export the configuration
+window.firebaseConfig = firebaseConfig; 
